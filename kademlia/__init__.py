3edd303e224c5dfec3c6546dc12de6d98bfb5e9f
Harish Chandra Thuwal <hthuwal@gmail.com>
2017-08-15 22:01:23 +0530

    I have added some more kademlia rpc functions like ping, store etc.
    TODO: Iterative functions
    
    Use the following link for details of Kademlia:
    http://xlattice.sourceforge.net/components/protocol/kademlia/specs.html

--- kademlia/__init__.py
+++ kademlia/__init__.py
@@ -1,10 +1,25 @@
 import asyncio
-
-from functools import wraps
-
 import pickle
 import socket
 
+from functools import wraps
+
+from .routing_table import RoutingTable
+
+
+def remote(func):
+
+    @asyncio.coroutine()
+    @wraps(func)
+    def inner(*args, **kwargs):
+        instance, peer, *args = args
+        answer = yield from instance.request(peer, inner.remote_name, *args, **kwargs)
+        return answer
+
+    inner.remote_name = func.__name__
+    inner.reply_function = func
+
+    return inner
 
 class DatagramRPCProtocol(asyncio.DatagramProtocol):
 
@@ -71,3 +86,50 @@ class DatagramRPCProtocol(asyncio.DatagramProtocol):
         message = pickle.dumps(obj)
 
         self.transport.sendto(message, peer)
+
+
+class KademliaNode(DatagramRPCProtocol):
+
+    def __init__(self, alpha=3, k=20, identifier=None):
+
+        if identifier is None:
+            identifier = get_random_identifier()
+
+        self.identifier = identifier
+        self.routing_table = RoutingTable(self.identifier, k=k)
+        self.k = k
+        self.alpha = alpha
+        self.storage = {}
+
+        super(KademliaNode, self).__init__()
+
+    def request_received(self, peer, message_identifier, procedure_name, args, kwargs):
+        peer_identifier = args[0]
+        self.routing_table.update_peer(peer_identifier, peer)
+
+        super(KademliaNode, self).request_received(peer, message_identifier, procedure_name, args, kwargs)
+
+    def reply_received(self, peer, message_identifier, answer):
+        peer_identifier, answer = answer
+        self.routing_table.update_peer(peer_identifier, peer)
+
+        super(KademliaNode, self).reply_received(peer, message_identifier, answer)
+
+    @remote
+    def ping(self, peer, peer_identifier):
+        return (self.identifier, self.identifier)
+
+    @remote
+    def store(self, peer, peer_identifier, key, value):
+        self.storage[key] = value
+        return (self.identifier, True)
+
+    @remote
+    def find_node(self, peer, peer_identifier, key):
+        return (self.identifier, self.routing_table.find_closest_peers(key, excluding=peer_identifier))
+
+    @remote
+    def find_value(self, peer, peer_identifier, key):
+        if key in self.storage:
+            return (self.identifier, ('found', self.storage[key]))
+        return (self.identifier, ('notfound', self.routing_table.find_closest_peers(key, excluding=peer_identifier)))
