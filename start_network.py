6065c408ca77c057e0f0ff4792fc7daa5e6c1ca5
Shadab Zafar <dufferzafar0@gmail.com>
2017-08-22 21:57:26 +0530

    Fixup start_network: Add logging, remove useless functions

--- start_network.py
+++ start_network.py
@@ -1,49 +1,52 @@
 #!/usr/bin/python
 
-import time
-# from mininet.topo import Topo
+import os
+
+import config
+
 from mininet.net import Mininet
-from mininet.util import dumpNodeConnections
-from mininet.log import setLogLevel
 
 BOOT_PORT = 9000
 LOCALHOST = "127.0.0.1"
 MAX_NODES = 3
 
-def test():
+
+def cleanup_logs():
+    for f in os.listdir(config.LOG_DIR):
+        os.remove(f)
+
+
+def main():
     hosts = []
     net = Mininet()
-    s0 = net.addSwitch('s0') #central switch
+    s0 = net.addSwitch('s0')  # central switch
 
-    #adding hosts
+    # adding hosts
     for id in range(0, MAX_NODES):
         host = net.addHost('h%s' % id)
         net.addLink(host, s0)
         hosts.append(host)
 
-    c0 = net.addController('c0') #whats the role of controller?
+    net.addController('c0')
 
     net.start()
 
     # first node without bootstrap
-    cmd_out = hosts[0].cmd('xterm -hold -geometry 130x40+0+900 -e python3 start_node.py %s %d &' %(LOCALHOST,BOOT_PORT))
+    hosts[0].cmd(
+        'xterm -hold -geometry 130x40+0+900 -e python3 start_node.py %s %d &' % (LOCALHOST, BOOT_PORT)
+    )
 
     boot_ip = hosts[0].IP()
 
     # rest of the nodes
     port = BOOT_PORT+1
     for host in hosts[1:]:
-        # time.sleep(1)
-        # cmd = 'xterm -hold -geometry 130x40+0+900 -e python3 -u start_node.py %s %d %s %d &' %(LOCALHOST,port,boot_ip,BOOT_PORT)
-        # print cmd
-        # print boot_ip
-        host.cmd('xterm -hold -geometry 130x40+0+900 -e python3 -u start_node.py %s %d %s %d &' %(LOCALHOST,port,boot_ip,BOOT_PORT))
-        port+=1
+        host.cmd('xterm -hold -geometry 130x40+0+900 -e python3 -u start_node.py %s %d %s %d &' %
+                 (LOCALHOST, port, boot_ip, BOOT_PORT))
+        port += 1
 
     raw_input('Press enter to stop all nodes.')
     net.stop()
 
 if __name__ == '__main__':
-    # Tell mininet to print useful information
-    setLogLevel('info')
-    test()
+    main()
