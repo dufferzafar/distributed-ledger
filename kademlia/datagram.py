c38bb395eb2577e86d7ef19c022d7203d0763844
Harish <hthuwal@gmail.com>
2017-08-23 01:13:33 +0530

    Seperate DatagramRPCProtocol to seperate file datagram.py

---
+++ kademlia/datagram.py
@@ -0,0 +1,91 @@
+import asyncio
+import pickle
+import logging
+import socket
+
+from .utils import sha1_int, random_id 
+logger = logging.getLogger(__name__)
+
+class DatagramRPCProtocol(asyncio.DatagramProtocol):
+
+    def __init__(self, reply_timeout=5):
+
+        self.outstanding_requests = {}
+        self.reply_functions = self.find_reply_functions()
+        self.reply_timeout = reply_timeout
+
+        super(DatagramRPCProtocol, self).__init__()
+
+    # TODO: Hardcode the reply_functions dict?
+    def find_reply_functions(self):
+        return {
+            func.remote_name: func.reply_function
+            for func in self.__class__.__dict__.values()
+            if hasattr(func, 'remote_name')
+        }
+
+    def connection_made(self, transport):
+        logger.info('connection_made: %r', transport)
+
+        self.transport = transport
+
+    def datagram_received(self, data, peer):
+        logger.info('data_received: %r, %r', peer, data)
+
+        direction, message_identifier, *details = pickle.loads(data)
+
+        if direction == 'request':
+            procedure_name, args, kwargs = details
+            self.request_received(peer, message_identifier, procedure_name, args, kwargs)
+
+        elif direction == 'reply':
+            answer = details[0]
+            self.reply_received(peer, message_identifier, answer)
+
+    def request_received(self, peer, message_identifier, procedure_name, args, kwargs):
+        logger.info('received request from %r: %r(*%r, **%r) as message %r',
+                    peer, procedure_name, args, kwargs, message_identifier)
+
+        reply_function = self.reply_functions[procedure_name]
+        answer = reply_function(self, peer, *args, **kwargs)
+        self.reply(peer, message_identifier, answer)
+
+    def reply_received(self, peer, message_identifier, answer):
+        logger.info('received reply to message %r, answer %r', message_identifier, answer)
+
+        if message_identifier in self.outstanding_requests:
+            reply = self.outstanding_requests.pop(message_identifier)
+            reply.set_result(answer)
+
+    def reply_timed_out(self, message_identifier):
+        if message_identifier in self.outstanding_requests:
+            reply = self.outstanding_requests.pop(message_identifier)
+            reply.set_exception(socket.timeout)
+
+    def request(self, peer, procedure_name, *args, **kwargs):
+        message_identifier = random_id()
+
+        logger.info("sending request to %r: %r(*%r, **%r) as message %r",
+                    peer, procedure_name, args, kwargs, message_identifier)
+
+        reply = asyncio.Future()
+        self.outstanding_requests[message_identifier] = reply
+
+        loop = asyncio.get_event_loop()
+        loop.call_later(self.reply_timeout, self.reply_timed_out, message_identifier)
+
+        obj = ('request', message_identifier, procedure_name, args, kwargs)
+        message = pickle.dumps(obj, protocol=0)
+
+        self.transport.sendto(message, peer)
+
+        return reply
+
+    def reply(self, peer, message_identifier, answer):
+        logger.info("sending reply to %r: (%r, %r)",
+                    peer, message_identifier, answer)
+
+        obj = ('reply', message_identifier, answer)
+        message = pickle.dumps(obj, protocol=0)
+
+        self.transport.sendto(message, peer)
