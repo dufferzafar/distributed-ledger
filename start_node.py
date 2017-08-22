a481523cbdb680638fc84fa1e4844dc81f2abe55
Shadab Zafar <dufferzafar0@gmail.com>
2017-08-22 21:57:56 +0530

    Add logging to start_node
    Support for both stdout and a file

--- start_node.py
+++ start_node.py
@@ -1,16 +1,44 @@
-import sys
 import asyncio
+import logging
+import os
+import sys
+
+import config
 
 from kademlia import KademliaNode
+
+def setup_logging(node_id):
+
+    if not os.path.exists(config.LOG_DIR):
+        os.mkdir(config.LOG_DIR)
+
+    kademlia_logger = logging.getLogger('kademlia')
+    kademlia_logger.setLevel(logging.DEBUG)
+
+    stream_handler = logging.StreamHandler()
+    # stream_handler = logging.FileHandler('logs/%d.log' % node_id, "w")
+    stream_handler.setLevel(logging.DEBUG)
+
+    format_ = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+    stream_handler.setFormatter(format_)
+
+    kademlia_logger.addHandler(stream_handler)
 
 
 def start_a_node(sock_addr, bootstrap_addr=None):
 
-    print (sock_addr[0],sock_addr[1])# just to check running or not
+    # DEBUG:
+    print(sock_addr[0], sock_addr[1])
+
     loop = asyncio.get_event_loop()
 
     f = loop.create_datagram_endpoint(KademliaNode, local_addr=sock_addr)
     _, node = loop.run_until_complete(f)
+
+    # Setup logging once we have the ID
+    setup_logging(node.identifier)
+
+    print(node.identifier)
 
     # TODO: Will this work? Test!
     if bootstrap_addr:
@@ -22,13 +50,13 @@ def start_a_node(sock_addr, bootstrap_addr=None):
 if __name__ == '__main__':
 
     # TODO: Improved argument parsing via docopt or click
-    
-    if len(sys.argv)==5:
-        start_a_node(   
-            sock_addr=(sys.argv[1], int(sys.argv[2])),bootstrap_addr=(sys.argv[3], int(sys.argv[4]))
+
+    if len(sys.argv) == 5:
+        start_a_node(
+            sock_addr=(sys.argv[1], int(sys.argv[2])),
+            bootstrap_addr=(sys.argv[3], int(sys.argv[4]))
         )
     else:
         start_a_node(
-                  sock_addr=(sys.argv[1], int(sys.argv[2]))
+            sock_addr=(sys.argv[1], int(sys.argv[2]))
         )
-
