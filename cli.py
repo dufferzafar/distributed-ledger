035b9fabb777ec08c17729bca1e953d45a7fe740
Harish <hthuwal@gmail.com>
2017-08-25 19:20:06 +0530

    Add get and put functions to cli and imporve argument parsing

--- cli.py
+++ cli.py
@@ -4,6 +4,7 @@ import os
 import sys
 import signal
 import config
+import re  # regex
 
 from kademlia_dht import Node
 from aioconsole import ainput
@@ -11,12 +12,41 @@ from start_node import setup_logging
 
 
 async def cli(node):
+
     while True:
-        line = await ainput(">>> ")
-        line = line.strip()  # because reads the "\n" when you press enter
-        if line == 'my dht':
+        cmd = await ainput(">>> ")
+        cmd = cmd.strip()  # because reads the "\n" when you press enter
+
+        if len(cmd) == 0:
+            continue
+        # can the commands and arguments be handled in a better way?
+        args = re.findall(r'"([^"]*)"', cmd)  # listing all arguments (must be each double quotes)
+        cmd = cmd.split()[0]
+
+        if cmd == 'dht':
             print(node)
-        # TODO implement rest of the functions
+
+        elif cmd == 'routing_table':
+            print(node.routing_table)
+
+        elif cmd == 'put':
+            if (len(args) != 2):
+                print("Expected 2 arguments, %d given" % len(args))
+            else:
+                await node.put(args[0], args[1], False)
+
+        elif 'get' in cmd:
+            if (len(args) != 1):
+                print("Expected 1 argument, %d given" % len(args))
+            try:
+                value = await node.get(args[0], False)
+                print(value)
+            except KeyError:
+                print("Key not found")
+
+        else:
+            print("Please enter valid input.\nType help to see commands")
+        # TODO implement rest of the functions and help
 
 
 def start_node_with_cli(sock_addr):
