0405a1334ad8f49efbcfc86cf8035e5abf6165a2
Harish <hthuwal@gmail.com>
2017-08-28 00:46:10 +0530

    Add a Ledger Class

--- trans.py
+++ trans.py
@@ -1,4 +1,35 @@
 from utils import random_id
+import time
+
+
+class Ledger(object):
+
+    """
+    A bitcoin-like ledger - containing a list of transactions.
+
+    Contains a "genesis transaction" that each node is aware of.
+    """
+
+    def __init__(self, node_id):
+        self.node_id = node_id  # belong to which node
+        self.ledger = [Transaction(None, node_id, None, None, 100)]  # Genesis Transaction
+
+    def add_trans(self, tx):
+        self.ledger.append(tx)
+        self.ledger.sort(key=lambda tx: tx.tx_id)
+
+    def verify_trans(self, tx):
+        pass
+        # TODO method to verify a transaction based on existing ledger
+
+    def __iter__(self):
+        return iter(self.ledger)
+
+    def __repr__(self):
+        r = ""
+        for _ in self.ledger:
+            r += (repr(_) + "\n")
+        return r
 
 
 class Transaction(object):
@@ -19,3 +50,11 @@ class Transaction(object):
     def __repr__(self):
         return "%r %r %r" % (repr(self.tx_id), repr(self.sender), repr(self.receiver))
 
+
+if __name__ == '__main__':
+    tx1 = Transaction(None, "third", None, None, 100)
+    l = Ledger("second")
+    tx2 = Transaction(None, "first", None, None, 100)
+    l.add_trans(tx2)
+    l.add_trans(tx1)
+    print(l)
