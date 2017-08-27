dbbb5717aa5ca1196d7c58a28c89d3d27a8e2471
Shadab Zafar <dufferzafar0@gmail.com>
2017-08-27 15:00:31 +0530

    Add ECDSA signing and verification

--- utils.py
+++ utils.py
@@ -4,6 +4,9 @@ import random
 
 import ecdsa
 
+# This elliptic curve is used by bitcoin too
+CURVE = ecdsa.SECP256k1
+
 
 def sha1_int(key):
     if hasattr(key, 'encode'):
@@ -21,9 +24,11 @@ def random_id():
 
 
 def gen_pub_pvt():
-    """ Generate key-pair. """
+    """
+    Generate key-pair.
+    """
 
-    sk = ecdsa.SigningKey.generate(curve=ecdsa.SECP256k1)
+    sk = ecdsa.SigningKey.generate(curve=CURVE)
     vk = sk.get_verifying_key()
 
     pvt_key = binascii.hexlify(sk.to_string()).decode()
@@ -32,5 +37,27 @@ def gen_pub_pvt():
     return pub_key, pvt_key
 
 
+# msg must be a bytes object - NOT str/unicode
+def sign_msg(pvt_key, msg):
+    pvt_key = binascii.unhexlify(pvt_key.encode())
+    sk = ecdsa.SigningKey.from_string(pvt_key, curve=CURVE)
+
+    return sk.sign(msg)
+
+
+def verify_msg(pub_key, msg, sign):
+    pub_key = binascii.unhexlify(pub_key.encode())
+    vk = ecdsa.VerifyingKey.from_string(pub_key, curve=CURVE)
+
+    return vk.verify(sign, msg)
+
 if __name__ == '__main__':
-    print(gen_pub_pvt())
+
+    # Test out signing & verification
+    msg = b"Shadab Zafar"
+
+    pub, pvt = gen_pub_pvt()
+
+    sign = sign_msg(pvt, msg)
+
+    assert verify_msg(pub, msg, sign)
