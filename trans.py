a8dcb49d409c0dd583d28d1f425d8b1dc9ce1abb
Harish <hthuwal@gmail.com>
2017-08-28 00:45:25 +0530

    Make Changes to Transactii

--- trans.py
+++ trans.py
@@ -1,18 +1,21 @@
 from utils import random_id
 
 
-class Transaction:
+class Transaction(object):
 
-    def __init__(self, sender, receiver, witness, input_tx=None):
+    def __init__(self, sender, receiver, witness, amount, input_tx=None):
 
-        self.id = random_id()
-        self.input_tx = input_tx
+        self.tx_id = time.time()  # transaction id is time(for virtual synchrony)
+        self.input_tx = input_tx  # input transactions, None for Genesis
         self.sender = sender
         self.receiver = receiver
         self.witness = witness
+        self.amount = amount
+        self.spent = False
 
     def __eq__(self, other):
-        return self.id == other.id  # no need to compare other attributes id must be unique
+        return self.tx_id == other.tx_id  # no need to compare other attributes id must be unique
 
     def __repr__(self):
-        return repr(self.id)
+        return "%r %r %r" % (repr(self.tx_id), repr(self.sender), repr(self.receiver))
+
